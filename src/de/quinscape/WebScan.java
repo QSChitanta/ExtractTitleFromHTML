8a84709287664272d8540abffd5b1115d2d6b9cb
pchitanta <patrick.chitanta@quinscape.de>
2021-12-20 15:33:57 +0100

    method intended to extract links from links...

--- src/de/quinscape/WebScan.java
+++ src/de/quinscape/WebScan.java
@@ -21,7 +21,7 @@ public class WebScan {
     private String extractHtmlContent(String webAddress) throws IOException {
         String line;
         StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = readURL(webAddress);
+        BufferedReader bufferedReader = readUrl(webAddress);
         while ((line = bufferedReader.readLine()) != null) {
             stringBuilder.append(line);
         }
@@ -29,7 +29,7 @@ public class WebScan {
         return stringBuilder.toString();
     }
 
-    private BufferedReader readURL(String webAddress) throws IOException {
+    private BufferedReader readUrl(String webAddress) throws IOException {
         InputStream inputStream = getUrl(webAddress).openStream();
         return new BufferedReader(new InputStreamReader(inputStream));
     }
@@ -47,22 +47,38 @@ public class WebScan {
         }
     }
 
-    private void printAllLinks(String htmlContent) {
+    private void printAllLinks(String htmlContent) throws IOException {
         Pattern pattern = Pattern.compile("<a href=\"(.*?)\"");
         Matcher matcher = pattern.matcher(htmlContent);
-        int i = 0;
+        int counter = 0;
         while (matcher.find()) {
             String link = matcher.group(1);
-            if (!link.equals("#")){
-                System.out.println("Link: " + link);
-                i++;
+            if (isValidLink(link)){
+                System.out.println("Link " + counter + ": " + link);
+                counter++;
+                diveIntoExternalLink(link);
             }
         }
-        System.out.println("total:" + i);
+        System.out.println("Amount of Links: " + (counter-1));
+    }
+
+    private boolean isValidLink(String link) {
+        return !link.startsWith("#");
+    }
+
+    private void diveIntoExternalLink(String link) throws IOException {
+        if (link.startsWith("https://") || link.startsWith("http://")){
+            diveIntoLinkAndScanAgain(link);
+        }
+    }
+
+    private void diveIntoLinkAndScanAgain(String diveLink) throws IOException {
+        WebScan diveDeeper = new WebScan();
+        diveDeeper.extractHtmlContent(diveLink);
     }
 
     private String normalizeUrl(String link){
-        if (!link.startsWith("https://") && !link.startsWith("http://")){
+        if (link.startsWith("https://") && !link.startsWith("http://")){
             return "https://" + link;
         }
         return link;
